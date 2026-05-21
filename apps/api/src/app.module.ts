66c8c1dde63c8e2ddc6aaae427bd4998f3125df7
archiebaltazarparaintl <archierbaltazar.paraintl@gmail.com>
2026-05-21 18:06:03 +0800

    modified:   .env 	modified:   apps/api/src/app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './modules/auth/auth.module';
 
-import { MailService } from './common/mail/mail.service';
-
 @Module({
-  imports: [PrismaModule, AuthModule],
-  providers: [MailService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    AuthModule,
+  ],
 })
 export class AppModule {}
