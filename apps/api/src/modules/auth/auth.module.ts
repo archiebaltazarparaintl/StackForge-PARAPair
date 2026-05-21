66dd7f9c468d8d88a769e5f0f56e69fe9394a9cc
archiebaltazarparaintl <archierbaltazar.paraintl@gmail.com>
2026-05-21 18:01:37 +0800

    modified:   apps/api/src/modules/auth/auth.module.ts 	modified:   apps/api/src/modules/auth/services/auth/auth.service.ts

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 
 import { AuthController } from './controllers/auth.controller';
-import { AuthRepository } from './repositories/auth.repository';
+
 import { AuthService } from './services/auth/auth.service';
+
+import { AuthRepository } from './repositories/auth.repository';
+
+import { PrismaService } from '../../prisma/prisma.service';
+
+import { MailService } from '../../common/mail/mail.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, AuthRepository],
+
+  providers: [AuthService, AuthRepository, PrismaService, MailService],
 })
 export class AuthModule {}
--- apps/api/src/modules/auth/services/auth/auth.service.ts
+++ apps/api/src/modules/auth/services/auth/auth.service.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { BadRequestException, Injectable } from '@nestjs/common';
 
 import * as bcrypt from 'bcrypt';
