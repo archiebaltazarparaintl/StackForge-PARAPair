0959f7ba2241ef192dd9bf02c7754b9856b8cc34
archiebaltazarparaintl <archierbaltazar.paraintl@gmail.com>
2026-05-23 16:08:42 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is ahead of 'origin/master' by 15 commits.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   apps/api/src/modules/profile/interests/route.ts
    	modified:   apps/api/tsconfig.build.tsbuildinfo
    	modified:   packages/db/src/auth.js
    	modified:   packages/db/src/auth.js.map

--- packages/db/src/auth.js
+++ packages/db/src/auth.js
@@ -3,16 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUserFromCookie = getUserFromCookie;
-require("server-only");
-const headers_1 = require("next/headers");
+exports.verifyAccessToken = verifyAccessToken;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-async function getUserFromCookie() {
-    const token = (await (0, headers_1.cookies)()).get("token")?.value;
-    if (!token)
-        return null;
+const JWT_SECRET = process.env.JWT_SECRET || 'secret';
+async function verifyAccessToken(token) {
     try {
-        return jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET);
+        if (!token)
+            return null;
+        const decoded = jsonwebtoken_1.default.verify(token, JWT_SECRET);
+        return decoded;
     }
     catch {
         return null;
