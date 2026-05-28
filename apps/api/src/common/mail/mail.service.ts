7b554829f273596cc38beb5a3621a56488f6b1eb
archiebaltazarparaintl <archierbaltazar.paraintl@gmail.com>
2026-05-28 19:20:09 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch compiled-push
    Changes to be committed:
    	modified:   apps/api/src/common/mail/mail.service.ts

--- apps/api/src/common/mail/mail.service.ts
+++ apps/api/src/common/mail/mail.service.ts
@@ -17,6 +17,8 @@ export class MailService {
         pass: this.config.get<string>('EMAIL_PASS'),
       },
     });
+    console.log('EMAIL_USER:', process.env.EMAIL_USER);
+    console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
   }
 
   sendOtpEmail(email: string, otp: string) {
